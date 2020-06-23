1fc2396306574b9e2e3a67ec07704d103392b6c2
schormans <matthew.schormans.10@ucl.ac.uk>
2020-06-23 22:56:37 +0100

    add web mode details in init.el

--- init.el
+++ init.el
@@ -128,6 +128,16 @@
   (add-to-list 'company-backends 'company-web-slim)
   )
 
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
 
 ;;org-mode configuration
 
