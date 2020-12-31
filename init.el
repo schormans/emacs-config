4e956790b54c892d9f5547df4a3338a201fa105b
schormans <matthew.schormans.10@ucl.ac.uk>
2020-12-31 22:21:02 +0000

    deft, zetteldeft, deactivate buffer-move, some other tweaks

--- init.el
+++ init.el
@@ -13,21 +13,24 @@
  '(TeX-engine 'luatex)
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes '(misterioso))
+ '(custom-enabled-themes '(doom-gruvbox))
+ '(custom-safe-themes
+   '("d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" default))
  '(display-line-numbers t)
  '(package-selected-packages
-   '(buffer-move company-web web-mode beacon zone-nyan company-auctex doom-themes irony-eldoc company-reftex company-jedi company-irony-c-headers company-irony company ein auctex))
+   '(zetteldeft deft doom-modeline helm buffer-move company-web web-mode beacon zone-nyan company-auctex doom-themes irony-eldoc company-reftex company-jedi company-irony-c-headers company-irony company ein auctex))
  '(word-wrap t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 119 :width normal :foundry "CYRE" :family "Fira Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 119 :width normal :family "IBM Plex Mono"))))
+ '(italic ((t (:slant italic :weight light)))))
 
 
 ;;try using a doom theme
-(load-theme 'doom-gruvbox t)
+;;(load-theme 'doom-gruvbox t)
 
 ;;Manually load my scripts from ~/.emacs.d/.
 
@@ -52,7 +55,7 @@
 (eval-when-compile
   (require 'use-package))
 
-;;my edits
+;;my basic edits
 
 (show-paren-mode 1)
 (setq inhibit-startup-message t)
@@ -70,6 +73,59 @@
 (setq reftex-plug-into-AUCTeX t)
 (setq TeX-parse-self t) ;enable parse on load
 (setq TeX-auto-save t) ;enable parse on save
+(setq reftex-allow-automatic-rescan t
+      reftex-enable-partial-scans t
+      reftex-save-parse-info t
+      reftex-use-multiple-selection-buffers t)
+
+
+;;doom-modeline
+;;if there are issues with this, do:
+;; M-x all-the-icons-install-fonts
+;;to ensure that the fonts that are required are downloaded and installed
+(use-package all-the-icons)
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+
+;;Helm configuration. Do a little config here, taking some inspiration from Matheus https://github.com/matman26/emacs-config
+
+(use-package helm
+  :init
+    (require 'helm-config)
+    (setq helm-split-window-in-side-p t
+          helm-move-to-line-cycle-in-source t)
+  :config 
+    (helm-mode 1) ;; Most of Emacs prompts become helm-enabled
+    (helm-autoresize-mode 1) ;; Helm resizes according to the number of candidates
+    (global-set-key (kbd "C-x b") 'helm-buffers-list) ;; List buffers ( Emacs way )
+    (global-set-key (kbd "C-x r b") 'helm-bookmarks) ;; Bookmarks menu
+    (global-set-key (kbd "C-x C-f") 'helm-find-files) ;; Finding files with Helm
+    (global-set-key (kbd "M-c") 'helm-calcul-expression) ;; Use Helm for calculations
+    (global-set-key (kbd "C-s") 'helm-occur)  ;; Replaces the default isearch keybinding
+    (global-set-key (kbd "C-h a") 'helm-apropos)  ;; Helmized apropos interface
+    (global-set-key (kbd "M-x") 'helm-M-x)  ;; Improved M-x menu
+    (global-set-key (kbd "M-y") 'helm-show-kill-ring)  ;; Show kill ring, pick something to paste
+  :ensure t)
+
+;;deft and zetteldeft
+;;setting default deft extension to org, likely just want .org zettels?
+
+(use-package deft
+  :ensure t
+  :bind ("<f8>" . deft)
+  :commands (deft)
+  :config (setq deft-directory "~/ownCloud/deft"
+		deft-extensions '("md" "org" "tex" "txt")
+		deft-recursive t
+		deft-default-extension "org"))
+
+(use-package zetteldeft
+  :ensure t
+  :after deft
+  :config (zetteldeft-set-classic-keybindings))
+
 
 ;;company autocomplete settings
 ;much of this is copied from cestlaz.github.io
@@ -104,18 +160,34 @@
 	     (add-hook 'python-mode-hook 'jedi:setup))
 
 (use-package company-reftex
-  :after latex auctex company
+  :after company
   :ensure t
   :config
   (add-to-list 'company-backends 'company-reftex-citations)
   (add-to-list 'company-backends 'company-reftex-labels)
-  ;(add-hook 'LaTeX-mode-hook 'company-reftex-citations)
-  ;(add-hook 'LaTeX-mode-hook 'company-reftex-labels)
+  (add-hook 'LaTeX-mode-hook 'company-reftex-citations)
+  (add-hook 'LaTeX-mode-hook 'company-reftex-labels)
   )
 
+(defun which-active-modes ()
+  "Return a list of which minor modes are enabled in the current buffer."
+  (interactive)
+  (let ((active-modes))
+    (mapc (lambda (mode) (condition-case nil
+                             (if (and (symbolp mode) (symbol-value mode))
+                                 (add-to-list 'active-modes mode))
+                           (error nil) ))
+          minor-mode-list)
+    active-modes))
+
+(defun my/reftex-after-save-fun ()
+  ;;this func is to be run after saving, check if reftex-mode is active, if so, refresh reference db
+  (if (member 'reftex-mode (which-active-modes)) (reftex-reset-mode) nil))
+
+(add-hook 'after-save-hook 'my/reftex-after-save-fun)
 
 (use-package company-auctex
-  :after latex auctex company
+  :after company
   :ensure t
   :config
   (add-to-list 'company-backends 'company-auctex))
@@ -165,10 +237,10 @@
 
 ;;buffer-move keybindings
 
-(require 'buffer-move)
+;; (require 'buffer-move)
 
-(global-set-key (kbd "<C-S-up>")    'buf-move-up)
-(global-set-key (kbd "<C-S-down>")  'buf-move-down)
-(global-set-key (kbd "<C-S-left>")  'buf-move-left)
-(global-set-key (kbd "<C-S-right>") 'buf-move-right)
+;; (global-set-key (kbd "<C-S-up>")    'buf-move-up)
+;; (global-set-key (kbd "<C-S-down>")  'buf-move-down)
+;; (global-set-key (kbd "<C-S-left>")  'buf-move-left)
+;; (global-set-key (kbd "<C-S-right>") 'buf-move-right)
 
