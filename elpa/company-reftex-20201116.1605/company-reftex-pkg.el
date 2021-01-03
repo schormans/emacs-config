14455617b0ac7231f5a32029fc02ae99880755c7
schormans <matthew.schormans.10@ucl.ac.uk>
2021-01-03 20:56:35 +0000

    upgrade all packages

---
+++ elpa/company-reftex-20201116.1605/company-reftex-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/myco/.emacs.d/elpa/company-reftex-20201116.1605/company-reftex.el  -*- no-byte-compile: t -*-
+(define-package "company-reftex" "20201116.1605" "Company backend based on RefTeX." '((emacs "25.1") (s "1.12") (company "0.8")) :commit "291c283c8a015fd7cbaa99f836e1a721f1e2c832" :authors '(("Eivind Fonn" . "evfonn@gmail.com")) :maintainer '("Eivind Fonn" . "evfonn@gmail.com") :keywords '("bib" "tex" "company" "latex" "reftex" "references" "labels" "citations") :url "https://github.com/TheBB/company-reftex")
